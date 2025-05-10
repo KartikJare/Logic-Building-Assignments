626fdf4fe12b883c7683ebc255737155f36d306b
KartikJare <kartikgjare@gmail.com>
2025-05-10 17:42:20 +0530

    Add files via upload

---
+++ Assignment_1/programAQ3.c
@@ -0,0 +1,30 @@
+///////////////////////////////////////////////////////////
+//
+//  File name : Assigment1Question3.c
+//  Descreption : Used to print 5 to 1 numbers on screen
+//  Author : Kartik Ganesh Jare
+//  Date : 09/05/25
+//  
+///////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Diplay()
+{
+    int i = 0;
+
+    i = 5;
+     
+    while(i >=1)
+    {
+        printf("%d\n", i);
+        i--;
+    }
+}
+
+int main()
+{
+    Diplay();
+
+    return 0;
+}
